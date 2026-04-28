dac9320c922fd21a224faa010d354d73b29b49c7
hhihn <heinkehihn@gmail.com>
2026-04-28 22:28:15 +0200

    build tcn blocks

--- architecture/tcn.py
+++ architecture/tcn.py
@@ -93,8 +93,6 @@ class TemporalConvolutionalNetwork(keras.Model):
                 inputs=inputs,
                 filters=filters_list[i],
                 block_idx=i,
-                pooling_size=pooling_size,
-                pooling_stride=2,
             )
             inputs = new_inputs
             self.tcn_blocks.append(block)
@@ -189,7 +187,7 @@ class TemporalConvolutionalNetwork(keras.Model):
         return keras.Model(inputs=inputs, outputs=x, name=f"cnn_block_{block_idx}"), x
 
     def _build_tcn_block(
-        self, inputs, filters: int, dilation_rate: int, block_idx: int
+        self, inputs, filters: int, block_idx: int
     ) -> tuple[Model, Any]:
         """Build a single TCN block with residual connection."""
 
@@ -197,7 +195,6 @@ class TemporalConvolutionalNetwork(keras.Model):
         x = keras.layers.Conv1D(
             filters,
             kernel_size=self.kernel_size,
-            dilation_rate=dilation_rate,
             padding="same",
             activation="elu",
             kernel_initializer="he_normal",
@@ -208,7 +205,6 @@ class TemporalConvolutionalNetwork(keras.Model):
         x = keras.layers.Conv1D(
             filters,
             kernel_size=self.kernel_size,
-            dilation_rate=dilation_rate,
             padding="same",
             activation="elu",
             kernel_initializer="he_normal",
