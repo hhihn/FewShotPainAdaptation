8455c598b2bec6766bc5550ed8343db97a6b8d71
hhihn <heinkehihn@gmail.com>
2026-05-03 09:11:34 +0200

    back to cnn blocks

--- architecture/tcn.py
+++ architecture/tcn.py
@@ -99,11 +99,12 @@ class TemporalConvolutionalNetwork(keras.Model):
             shape=(self.sequence_length, 1), name=f"tcn_block_{0}_input"
         )
         for i in range(num_blocks):
-            block, new_inputs = self._build_tcn_block(
+            block, new_inputs = self._build_cnn_block(
                 inputs=inputs,
                 filters=filters_list[i],
                 block_idx=i,
-                dilation_rate=dilation_rates[i],
+                pooling_stride=self.strides,
+                pooling_size=self.pooling_size,
             )
             inputs = new_inputs
             self.tcn_blocks.append(block)
